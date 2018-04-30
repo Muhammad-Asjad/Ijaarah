2fb08a7c05ff1631d379667d7a8e660a4046efe9
Muhammad-Asjad <38669787+Muhammad-Asjad@users.noreply.github.com>
2018-04-30 12:12:33 +0500

    Add files via upload

--- components/post-add/post-add.component.ts
+++ components/post-add/post-add.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { AdService, AdListing } from './ad.service';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-post-add',
@@ -9,20 +13,38 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./post-add.component.css']
 })
 export class PostAddComponent {
-  category: any[]
-  ad_title: any[]
-  ad_description: any[]
-  price: any[]
 
-  constructor(private db: AngularFireDatabase, public afAuth: AngularFireAuth) { }
-
-  private addUsersData(category: string, ad_title: string, ad_description: string, price: string) {
-    const itemsRef = this.db.list('/ads');
-    itemsRef.push({ category: category, ad_title: ad_title, ad_description: ad_description, price: price });
+  postAdd: FormGroup;
+  constructor(public fb: FormBuilder, private db: AngularFireDatabase, public afAuth: AngularFireAuth) {
+    this.createForm();
   }
- /*  createAd() {
-    const adDefault = new AdListing()
-    const adKey = this.db.list('/ads').push(adDefault).key
-    return this.db.object('/ads/' + adKey)
-  }*/
+
+
+  createForm() {
+    this.postAdd = this.fb.group({
+      Category: ['', [Validators.required, Validators.minLength(100)]],
+      title: ['', [Validators.required, Validators.minLength(50)]],
+      Description: ['', [Validators.required, Validators.minLength(255)]],
+      price: ['', [Validators.required, Validators.minLength(13)]],
+      name: ['', [Validators.required, Validators.minLength(13)]],
+      email: ['', [Validators.required, Validators.minLength(50)]],
+      phone: ['', [Validators.required, Validators.minLength(13)]],
+      password: ['', [Validators.required, Validators.minLength(13)]],
+      image: ['', [Validators.required]],
+
+    })
+    
+  }
+
+/*
+  fetchData() {
+    console.log(this.postAdd.value);
+        //this.addUsersData();    
+  }
+*/
+  private addUsersData() {
+    console.log(this.postAdd.value)
+    const itemsRef = this.db.list('/Posted_Ads');
+    itemsRef.push( this.postAdd.value );
+  }
 }
